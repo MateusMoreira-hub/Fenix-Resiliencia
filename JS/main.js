3ac28d8e4ffdeb5b9fc8ee381988efdb63feceef
Mateus Moreira <mateusmoreiraemail@gmail.com>
2026-08-16 22:17:30 -0300

    Update 1

--- JS/main.js
+++ JS/main.js
@@ -5,8 +5,18 @@ word3 = document.querySelector("#word3")
 word4 = document.querySelector("#word4")
 cond = [0,0,0,0]
 
+  function atualizarContagem() {
+  if (segundos > 0) {
+    segundos--;
+    setTimeout(atualizarContagem, 1000); 
+  } else{
+window.location.href = "/HTML/burnwords.html";
+}
+  }
+  
+
 function queimar1(){
-word1.innerHTML = "<h1>Eu sou capaz</h1>";    
+word1.innerHTML = "<h2>Eu sou capaz</h2>";    
 word1.style.color="yellow";
 word1.style.animation = "txtflame 2s linear infinite";
 word1.style.filter= "drop-shadow(0 0 10px rgba(255, 234, 0, 0.8))";
@@ -15,7 +25,7 @@ verautoestima();
 }
 
 function queimar2(){
-word2.innerHTML = "<h1>Eu sou incrível</h1>";    
+word2.innerHTML = "<h2>Eu sou incrível</h2>";    
 word2.style.color="yellow";
 word2.style.animation = "txtflame 2s linear infinite";
 word2.style.filter= "drop-shadow(0 0 10px rgba(255, 234, 0, 0.8))";
@@ -24,7 +34,7 @@ verautoestima();
 }
 
 function queimar3(){
-word3.innerHTML = "<h1>Eu vou fazer agora</h1>";    
+word3.innerHTML = "<h2>Eu vou fazer agora</h2>";    
 word3.style.color="yellow";
 word3.style.animation = "txtflame 2s linear infinite";
 word3.style.filter= "drop-shadow(0 0 10px rgba(255, 234, 0, 0.8))";
@@ -33,7 +43,7 @@ verautoestima();
 }
 
 function queimar4(){
-word4.innerHTML = "<h1>Eu sou amável</h1>";    
+word4.innerHTML = "<h2>Eu sou amável</h2>";    
 word4.style.color="yellow";
 word4.style.animation = "txtflame 2s linear infinite";
 word4.style.filter= "drop-shadow(0 0 10px rgba(255, 234, 0, 0.8))";
@@ -44,10 +54,22 @@ verautoestima();
 function verautoestima(){
 autoestima = cond[0]+cond[1]+cond[2]+cond[3]
 if (autoestima == 4){
-    setTimeout(proxpage, 500)
+hum = document.querySelector("#hum");
+fenix = document.createElement("img");
+fenix.className="fenix";
+fenix.src="/IMAGE/Fenix.png";
+hum.appendChild(fenix);
+fundo.style.animation="renascimento 2s linear"
+fenix.style.animation="voando 5s linear"
+    setTimeout(proxpage, 3000)
 }
 }
 
+function queimarword(){
+    word = document.querySelector("#yourword");
+}
+
+
 function proxpage(){
-window.location.href = "/HTML/renascendo.html"
+window.location.href = "/HTML/yourword.html";
 }
